f4819ea59b7c5b75ce16bdba3b8a77d151f13082
Work Goonja <workgoonja@goonjas-MacBook-Pro.local>
2024-06-28 23:35:00 -0400

    version miss match but still trying

--- src/service/weather.service.ts
+++ src/service/weather.service.ts
@@ -1,5 +1,3 @@
-import { Iweather } from "../model/weather.class";
-
 const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
 const API_ID = "017dde2927e030d8dc2069f101b19b37";
 const UNIT = "metric";
